9efd7129fc275c0f98fcd23f2bb1fe86e6063eb1
sayed-shidqy <sayedshidqyali@gmail.com>
2026-04-21 00:00:41 +0700

    Fix typo GeminiResponse struct

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ type Message struct {
 }
 
 // Struktur untuk request ke Gemini
-type GeminiRequest struct {
-	Contents []struct {
-		Parts []struct {
-			Text string `json:"text"`
-		} `json:"parts"`
-	} `json:"contents"`
+type GeminiResponse struct {
+	Candidates []struct {
+		Content struct {
+			Parts []struct {
+				Text string `json:"text"`
+			} `json:"parts"`
+		} `json:"content"`
+	} `json:"candidates"`
 }
 
 // Struktur untuk respon dari Gemini
